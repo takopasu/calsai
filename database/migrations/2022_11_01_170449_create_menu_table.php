65afe8d3997040f23cca4d6cea739fc189c99126
takopasu <s22mf11@ge.osaka-sandai.ac.jp>
2022-11-02 02:50:39 +0900

    first commit

---
+++ database/migrations/2022_11_01_170449_create_menu_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu', function (Blueprint $table) {
+            $table->id();
+            $table->string('menu_name');
+            $table->string('category');
+            $table->string('type');
+            $table->string('order_num');
+            $table->integer('price');
+            $table->integer('kcal');
+            $table->float('solt');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu');
+    }
+};
