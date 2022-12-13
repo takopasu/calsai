6fd582f92b092613690cb5619822cb70aaec5066
takopasu <s22mf11@ge.osaka-sandai.ac.jp>
2022-12-13 16:29:08 +0900

    全機能完成_ver1.0_20221213

--- resources/views/menus/save.blade.php
+++ resources/views/menus/save.blade.php
@@ -3,7 +3,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>保存内容</title>
+        <title>保存結果</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600"  rel="stylesheet">
@@ -11,23 +11,22 @@
     </head>
     <body class='antialiased'>
         <br>
-        <h1 class="title">保存内容</h1>
+        <h1 class="title">全件表示</h1>
         <hr>
             <div class='body'>
             <a href='/only_favorite' class='url link body'>お気に入りのみはこちら</a>
             <br>
             
+            <div class='inline_block'>
             @foreach($saves as $save)
-                @if($save["is_saved"] === 0)
-                @else
-                    <div class='box3'>
-                        <form action='/show' method='POST'>
-                            @csrf
-                            <button type='submit' name='show' class='show' value ="{{$save['id']}}">保存日時: {{ $save->created_at }}</button>
-                        </form>
-                    </div>
+                @if($save["is_saved"] === 1)
+                    <form action='/show' method='POST'>
+                        @csrf
+                        <button type='submit' name='show' class='show box3' value ="{{$save['id']}}">保存日時: {{ $save->created_at }}</button>
+                    </form>
                 @endif
             @endforeach
+            </div>
         </div>
     </body>
 </html>
